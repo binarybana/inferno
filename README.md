6ac489cc9b4f1457eec28e534b0aced28f486d37
Jason Knight <binarybana@gmail.com>
2025-01-05 10:32:53 -0800

    Use actual brightness values in ember bed

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Inferno
 
-Controlling  my digital fireplace
+Controlling my digital fireplace
 
 ## Installation
 
 Place the `.desktop` file in the `$HOME/.config/autostart/fireplace.desktop` location.
+
+## Controlling the microcontroller
+
+Launch the standalone embers program.
+```
+uvx mpremote run src/standalone_tests/embers.py
+```
--- src/client_server_tests/pico.py
+++ src/client_server_tests/pico.py
@@ -1,63 +1,99 @@
 import time
-from machine import Pin, PWM, Timer
+import struct
+import asyncio
+
+from typing import List, Tuple, Iterable
+
+from machine import Pin, PWM
 import select
 import sys
-import time
 
 
 led = PWM(Pin("LED"))
 ember = PWM(Pin("GP16"))
-tim = Timer()
 relay = Pin("GP0", Pin.OUT)
-duty = 0
-direction = 1
+relay.value(0)
+
+DATA_CAP = 10000
+
 
 ember.freq(1000)
 led.freq(1000)
 
 
-def tick(timer):
-    global relay
-    relay.toggle()
+def load_brightness_data(fname: str) -> Tuple[float]:
+    with open(fname, "rb") as f:
+        # Read the number of values
+        num_values = struct.unpack("!I", f.read(4))[0]
+        num_values = min(num_values, DATA_CAP)
+        # Read each brightness value
+        return struct.unpack("!" + str(num_values) + "f", f.read(4 * num_values))
 
 
-def update_pwm():
-    global duty, direction
-    duty += direction
-    if duty > 255:
-        duty = 255
-        direction = -1
-    elif duty < 0:
-        duty = 0
-        direction = 1
-    led.duty_u16(duty * duty)
-    ember.duty_u16(duty * duty)
-    time.sleep(0.001)
+def scale_brightness_data(
+    values: Iterable[float], max_brightness: float, dynamic_range: float
+) -> List[int]:
+    """
+    max_brightness: float between 0 and 1 indicating how bright the user wants the bed to ever reach
+    dynamic_range: float between 0 and 1 indicating how much of the remaining portion of usable output the user wants to use
+    """
+    min_val = min(values)
+    max_val = max(values)
+    max_desired_val = max_brightness * (2**16 - 1)
+    min_desired_val = max_desired_val * dynamic_range
+    observed_range = max_val - min_val
+    desired_range = max_desired_val - min_desired_val
+
+    scale_factor = desired_range / observed_range
+    shift_factor = (max_desired_val * (1 - dynamic_range)) - (min_val * scale_factor)
+    # print(f"{min_val=} {max_val=} {scale_factor=} {shift_factor=}")
+    return [int(shift_factor + scale_factor * x) for x in values]
+
+
+async def update_pwm():
+    brightess_lut = scale_brightness_data(
+        load_brightness_data("packed_brightness.bin"),
+        max_brightness=1.0,
+        dynamic_range=0.8,
+    )
+    time_index = 0
+    sleep_time = 1 / 60.0
+    while True:
+        # if time_index % 60 == 0:
+        #     print(".", end="")
+        brightness = brightess_lut[time_index]
+        led.duty_u16(brightness)
+        ember.duty_u16(brightness)
+        await asyncio.sleep(sleep_time)
+        time_index += 1
+        time_index %= len(brightess_lut)
 
 
 # Set up the poll object
 poll_obj = select.poll()
 poll_obj.register(sys.stdin, select.POLLIN)
 
-# Start tick timer
-# tim.init(freq=0.2, mode=Timer.PERIODIC, callback=tick)
 
-# Loop indefinitely
-while True:
-    # Wait for input on stdin
-    poll_results = poll_obj.poll(
-        1
-    )  # the '1' is how long it will wait for message before looping again (in microseconds)
-    if poll_results:
-        # Read the data from stdin (read data coming from PC)
-        data = sys.stdin.readline().strip()
-        if data == "on":
-            relay.value(1)
-        if data == "off":
-            relay.value(0)
-        # Write the data to the input file
-        sys.stdout.write("received data: " + data + "\r")
-    else:
-        # do something if no message received (like feed a watchdog timer)
-        update_pwm()
-        continue
+async def control_loop():
+    while True:
+        poll_results = poll_obj.poll(1)
+        if poll_results:
+            data = sys.stdin.readline().strip()
+            if data == "on":
+                relay.value(1)
+            if data == "off":
+                relay.value(0)
+            sys.stdout.write("received data: " + data + "\r")
+        else:
+            await asyncio.sleep(0.1)
+
+
+async def main():
+    task1 = asyncio.create_task(control_loop())
+    task2 = asyncio.create_task(update_pwm())
+    await task1
+    await task2
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
