828cc1fe4cffd1e327d09243a6efa0ccd277205d
Jason Knight <392846+binarybana@users.noreply.github.com>
2025-06-23 14:28:48 -0700

    Spawn MPV processes on both monitors

--- headless/src/main.rs
+++ headless/src/main.rs
@@ -23,15 +23,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = Args::parse();
     
-    // Start MPV video playback
+    // Start MPV video playback on both monitors
     let socket_path = args.mpv_socket.unwrap_or("/tmp/mpvsocket".into());
-    let _mpv_process = start_video_playback(&socket_path)?;
-    println!("MPV video playback started");
+    let mut vid_player = VidPlayerState::new(&socket_path);
+    vid_player.start_playback()?;
+    println!("MPV video playback started on both monitors");
     
-    // Wait a moment for MPV to start and create the socket
+    // Wait a moment for MPV to start and create the sockets
     thread::sleep(time::Duration::new(2, 0));
-    let mut mpv_socket = UnixStream::connect(&socket_path)?;
-    println!("mpv socket opened.");
+    vid_player.connect_sockets()?;
 
     // List available devices
     println!("Listing all available libev devices.");
@@ -67,11 +67,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     0 => println!("Key released: {:?}", key),
                     1 => {
                         println!("Key pressed: {:?}", key);
-                        mpv_socket.write_all(format!("show-text {:?}/n", key).as_bytes())?;
-                        mpv_socket.flush()?;
+                        vid_player.send_message(&format!("show-text {:?}/n", key))?;
                         match key {
                             Key::KEY_INSERT => heater.heater_on()?,
                             Key::KEY_DELETE => heater.heater_off()?,
+                            Key::KEY_ESC => {
+                                println!("Escape pressed - shutting down");
+                                vid_player.shutdown()?;
+                                return Ok(());
+                            }
                             _ => (),
                         }
                     }
@@ -83,21 +87,90 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 }
 
-fn start_video_playback(socket_path: &str) -> Result<Child, Box<dyn std::error::Error>> {
-    // Get the home directory for fire_videos path
-    let home_dir = std::env::var("HOME").unwrap_or_else(|_| "/home/jason".to_string());
-    let video_path = format!("{}/fire_videos/*.webm", home_dir);
-    
-    println!("Starting MPV with video path: {}", video_path);
-    
-    let child = Command::new("mpv")
-        .arg("--fs")
-        .arg(format!("--input-ipc-server={}", socket_path))
-        .arg("--loop-file")
-        .arg(&video_path)
-        .spawn()?;
-    
-    Ok(child)
+struct VidPlayerState {
+    mpv_monitor0: Option<Child>,
+    mpv_monitor1: Option<Child>,
+    socket_path_0: String,
+    socket_path_1: String,
+    socket_0: Option<UnixStream>,
+    socket_1: Option<UnixStream>,
+}
+
+impl VidPlayerState {
+    fn new(base_socket_path: &str) -> Self {
+        Self {
+            mpv_monitor0: None,
+            mpv_monitor1: None,
+            socket_path_0: format!("{}_0", base_socket_path),
+            socket_path_1: format!("{}_1", base_socket_path),
+            socket_0: None,
+            socket_1: None,
+        }
+    }
+
+    fn start_playback(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let home_dir = std::env::var("HOME").unwrap_or_else(|_| "/home/jason".to_string());
+        let video_path = format!("{}/fire_videos/*.webm", home_dir);
+        
+        println!("Starting MPV instances with video path: {}", video_path);
+        
+        // Start MPV for monitor 0
+        let child0 = Command::new("mpv")
+            .arg("--fs")
+            .arg("--fs-screen=0")
+            .arg(format!("--input-ipc-server={}", self.socket_path_0))
+            .arg("--loop-file")
+            .arg(&video_path)
+            .spawn()?;
+        
+        // Start MPV for monitor 1
+        let child1 = Command::new("mpv")
+            .arg("--fs")
+            .arg("--fs-screen=1")
+            .arg(format!("--input-ipc-server={}", self.socket_path_1))
+            .arg("--loop-file")
+            .arg(&video_path)
+            .spawn()?;
+        
+        self.mpv_monitor0 = Some(child0);
+        self.mpv_monitor1 = Some(child1);
+        
+        Ok(())
+    }
+
+    fn connect_sockets(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.socket_0 = Some(UnixStream::connect(&self.socket_path_0)?);
+        self.socket_1 = Some(UnixStream::connect(&self.socket_path_1)?);
+        println!("Connected to both MPV sockets");
+        Ok(())
+    }
+
+    fn send_message(&mut self, msg: &str) -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(ref mut socket) = self.socket_0 {
+            socket.write_all(msg.as_bytes())?;
+            socket.flush()?;
+        }
+        if let Some(ref mut socket) = self.socket_1 {
+            socket.write_all(msg.as_bytes())?;
+            socket.flush()?;
+        }
+        Ok(())
+    }
+
+    fn shutdown(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(mut process) = self.mpv_monitor0.take() {
+            let _ = process.kill();
+            let _ = process.wait();
+        }
+        if let Some(mut process) = self.mpv_monitor1.take() {
+            let _ = process.kill();
+            let _ = process.wait();
+        }
+        self.socket_0 = None;
+        self.socket_1 = None;
+        println!("Both MPV instances terminated");
+        Ok(())
+    }
 }
 
 enum HeaterState {
